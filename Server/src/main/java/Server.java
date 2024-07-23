d65892846f6ead553de44ebb73dc53d3281c47e4
DmitriyV <dima1999-91@mail.ru>
2024-07-23 23:04:19 +0300

    0004 Client Server

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -7,17 +7,35 @@ public class Server {
     public static void main(String[] args) {
 
        try (ServerSocket server = new ServerSocket( 8000 )) {
+
            System.out.println("Start Server");
 
-           try (
-           Socket socket = server.accept();
-           BufferedWriter writer =
-                   new BufferedWriter(
-                           new OutputStreamWriter(socket.getOutputStream()))) {
-               writer.write("Добро пожаловать");
-               writer.newLine();
-               writer.flush();
-           }
+           while (true)
+               try (
+                    Socket socket = server.accept();
+                    BufferedWriter writer =
+                            new BufferedWriter(
+                                new OutputStreamWriter(
+                                    socket.getOutputStream()));
+                    BufferedReader reader =
+                            new BufferedReader(
+                                new InputStreamReader(
+                                    socket.getInputStream()));
+               ) {
+
+
+                   String request = reader.readLine();
+                   if (request == null) {
+                       System.out.println("НИЧЕГО");
+                   } else {
+
+                       String response = "Добро пожаловать " + request.length();
+                       System.out.println(request);
+                       writer.write(response);
+                       writer.newLine();
+                       writer.flush();
+                   }
+               }
 
 
        } catch (IOException e){
